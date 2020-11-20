d7cbcd6fa563f42712d8bcf0e52829439a4a62aa
Peter Mezei <mezeipetister@gmail.com>
2020-11-20 06:47:55 +0100

    refactored

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,41 @@ use chrono::prelude::*;
 use document::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let cashin = templates::cash_in::CashIn::init(29)
-        .unwrap()
-        .set_data(templates::cash_in::Data::new(
-            "Peter Mezei".into(),
-            "4551 Nyíregyháza, Mogyorós utca 36.".into(),
-            "Mezeiné B. Krisztina".into(),
-            "4551 Nyíregyháza, Mogyorós utca 36.".into(),
-            "2020-11-18 17:29".into(),
-            75000,
-        ))
-        .unwrap()
-        .create_document()
-        .unwrap()
-        .get_document_base64()
-        .unwrap();
-    println!("Base64 is => {}", cashin);
+    let provider = document::Provider::init("GRDN", templates::cash_in::CashIn::default());
+
+    let data_object = templates::cash_in::CashIn::new(
+        "Peter Mezei".into(),
+        "4551 Nyíregyháza, Mogyorós utca 36.".into(),
+        "Mezeiné B. Krisztina".into(),
+        "4551 Nyíregyháza, Mogyorós utca 36.".into(),
+        "2020-11-18 17:29".into(),
+        75000,
+    );
+
+    let data_str = serde_json::to_string(&data_object).unwrap();
+
+    let id = provider.create_document(&data_str).unwrap();
+
+    println!("ID is => {}", id.to_string());
+
+    println!(
+        "Base 64 is => {}",
+        provider.get_document_base64("GRDN-2020-1").unwrap()
+    );
+
     Ok(())
+}
+
+mod v2 {
+    use std::sync::Mutex;
+    pub struct Providers {
+        cash_in: (),
+        cash_out: (),
+        inventory_log: (),
+        procurement: (),
+    }
+
+    pub struct DocumentService {
+        providers: Mutex<Providers>,
+    }
 }
--- src/templates/cash_in.rs
+++ src/templates/cash_in.rs
@@ -1,8 +1,8 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Data {
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct CashIn {
     payee_name: String,
     payee_address: String,
     receiver_name: String,
@@ -11,7 +11,7 @@ pub struct Data {
     amount: i32,
 }
 
-impl Data {
+impl CashIn {
     pub fn new(
         payee_name: String,
         payee_address: String,
@@ -31,33 +31,8 @@ impl Data {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct CashIn {
-    id: u32,
-    data: Option<Data>,
-}
-
-impl crate::document::DocumentProvider<Data> for CashIn {
-    fn init(id: u32) -> Result<Self, crate::document::DocumentError> {
-        Ok(CashIn { id, data: None })
-    }
-
-    fn set_data(mut self, data: Data) -> Result<Self, crate::document::DocumentError> {
-        self.data = Some(data);
-        Ok(self)
-    }
-
-    fn get_data(&self) -> Option<Data> {
-        self.data.clone()
-    }
-
+impl crate::document::DocumentProvider for CashIn {
     fn get_location(&self) -> &'static str {
         "cash_in"
-    }
-    fn get_id(&self) -> u32 {
-        self.id
-    }
-    fn get_prefix(&self) -> &'static str {
-        "bga"
     }
 }
